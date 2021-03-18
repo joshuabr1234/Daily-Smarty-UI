97e9e3d2bcc950fa0b185160d9637020d9379d51
Josh Baker <joshuabr1234@yahoo.com>
2021-03-18 07:28:17 -0500

    enabled recent and results posts tree

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,20 +1,31 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
-import recentPosts from '../components/recentPosts';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
 export default function(state = INIT_STATE, action) {
-    swtich (action.type) {
-        case SET_RECENT_POSTS:
+    swtich (action.type)  {
 
-            return[...state, recentPosts: action.payload]
+        case SET_RECENT_POSTS:
+            const recentPosts = action.payload;
+            return {
+                ...state, 
+                recentPosts
+            }
+
+            case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return {
+                ...state, 
+                resultsPosts
+            }
 
         default:
             return state;
     }
-}
+} 
