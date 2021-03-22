de50b63a4e43e33013dc63c620b14cc178bfd855
Joshua Baker <joshuabr1234@yahoo.com>
2021-03-22 06:38:02 -0500

    put together restulsposts component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,43 +1,32 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
-
 import * as actions from '../actions';
-
 import Post from './post';
-
 class RecentPosts extends Component {
-
     componentDidMount() {
         this.props.fetchRecentPosts();
     }
-
     renderPosts = function() {
         const posts = this.props.recentPosts.map((post, index) => {
-            if (index < 3) {
-                return  (
+            if(index < 3) {
+                return (
                     <Post {...post} key={index}/>
-                )
-            }            
+                )   
+            }
         })
-       
         return posts
-
     }
-
     render() {
         return (
-            <div className="recdnt-posts">
-                <div className="recent-posts-wrapper">
-                    <div className="recent-posts-heading">
-                        Recent Posts
-                    </div>
-                    <ul className="recent-posts-posts">
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
                         {this.renderPosts()}
                     </ul>
                 </div>
             </div>
-        );
+        )
     }
 }
 function mapStateToProps(state) {
@@ -45,6 +34,4 @@ function mapStateToProps(state) {
         recentPosts: state.posts.recentPosts
     }
 }
-
 export default connect(mapStateToProps, actions)(RecentPosts);
-            
